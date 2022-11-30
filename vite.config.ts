9334fd89c98246adfe70a9b99488c06931c610a0
e7a894 <e7a894@outlook.jp>
2022-11-30 08:27:25 +0000

    chore: fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: process.env.Node_ENV === 'production'
-    ? '/expt-vannila-ts/'
+    ? '/expt-vanilla-ts/'
     : './'
 });
